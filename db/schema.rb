5ed2a14b7e868452a0a40cd070994d4f512d6eb6
charlotte divine <divinemaina@gmail.com>
2022-09-14 05:37:25 -0700

    add foreign keys, index between user and likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_123138) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_123610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123138) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123138) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
