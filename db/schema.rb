ab168f74d9e09dc3d8ea5b68af3a5739e23c9a7b
charlotte divine <divinemaina@gmail.com>
2022-09-14 05:42:38 -0700

    add foreign keys, index between post and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_123934) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_124134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -51,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123934) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
