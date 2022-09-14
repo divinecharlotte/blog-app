da0489b65d2354f2d8611f43eec321381ea5973d
charlotte divine <divinemaina@gmail.com>
2022-09-14 13:57:09 -0700

    Add method to returns 5 recent comments for a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,13 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
-    
+
     after_save :update_posts_counter
 
     def update_posts_counter
       author.update(posts_counter: author.posts.count)
     end
+    def recent_five_comments
+        comments.last(5)
+      end
 end
