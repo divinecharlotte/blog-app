22bd85b5bb0917855f519aa286acf02857fbaa2e
charlotte divine <divinemaina@gmail.com>
2022-09-14 13:55:55 -0700

    A method that updates the posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+    
+    after_save :update_posts_counter
+
+    def update_posts_counter
+      author.update(posts_counter: author.posts.count)
+    end
 end
