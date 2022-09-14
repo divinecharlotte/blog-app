18d2fc7388a8af145924cf78be4a023bbf5ada0a
charlotte divine <divinemaina@gmail.com>
2022-09-14 05:40:41 -0700

    add foreign keys, index between post and likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_123610) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_123934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -50,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_123610) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
