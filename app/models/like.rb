e8d18733680e4956f0ff8afb97792b03070a141f
charlotte divine <divinemaina@gmail.com>
2022-09-14 17:49:51 -0700

    fix errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_save :update_likes_counter
+  after_save :update_likes_counter
 
-    def update_likes_counter
-      post.update(likes_counter: post.likes.count)
-    end
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    after_save :update_posts_counter
+  after_save :update_posts_counter
 
-    def update_posts_counter
-      author.update(posts_counter: author.posts.count)
-    end
-    def recent_five_comments
-        comments.last(5)
-      end
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_five_comments
+    comments.last(5)
+  end
 end
