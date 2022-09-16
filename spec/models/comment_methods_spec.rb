289dd5e59ada78ef2e777e05660e8f712b733a2b
charlotte divine <divinemaina@gmail.com>
2022-09-15 17:18:25 -0700

    add tests for comment model's methods

---
+++ spec/models/comment_methods_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  user = User.create(
+    name: 'Doe', photo: 'https://doe.com/me.png',
+    bio: 'Iam John Doe.', post_counter: 0
+  )
+
+  post = Post.create(
+    title: 'About', text: 'About me', comments_counter: 0,
+    likes_counter: 0, author_id: user.id
+  )
+
+  comment = Comment.create(post: post, author: user)
+
+  context 'update_comments_counter' do
+    comment.update_comments_counter
+
+    it ' incriment comments_counter' do
+      expect(Post.find(post.id).comments_counter).eql?(post.comments_counter + 1)
+    end
+  end
+end
