0b33bd222aafd6f9e63461da1b8f59d3a8fa870b
charlotte divine <divinemaina@gmail.com>
2022-09-20 13:12:34 -0700

    add tests for show of posts route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
+  
+  describe 'GET /show' do
+    before(:example) do
+      get '/users/1/posts/1'
+    end 
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Post content for a given user')
+    end
+  end
 end
