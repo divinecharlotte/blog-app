1b6a0f609c63ebace08816682665c24f67284d98
charlotte divine <divinemaina@gmail.com>
2022-09-22 23:34:45 -0700

    add forms

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
-    def create
-      @like = current_user.likes.new
-      @like.post_id = params[:post_id]
-      if @like.save
-        redirect_to user_post_likes_path(current_user, @like.post_id)
-      else
-        render :create
-      end
+  def create
+    @like = current_user.likes.new
+    @like.post_id = params[:post_id]
+    if @like.save
+      redirect_to user_post_likes_path(current_user, @like.post_id)
+    else
+      render :create
     end
   end
+end
