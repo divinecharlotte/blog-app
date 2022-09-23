ced1fdfd9bae92664517cc0b15a1bf165e26b281
charlotte divine <divinemaina@gmail.com>
2022-09-22 21:26:41 -0700

    fix some issues before starting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
-  end
+  def index; end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     before(:example) do
@@ -9,15 +8,14 @@ RSpec.describe 'Users', type: :request do
     it 'return correct response' do
       expect(response).to have_http_status(:ok)
     end
-
     it 'should render correct template' do
       expect(response).to render_template(:index)
     end
-
     it 'body should includes correct placeholder text' do
       expect(response.body).to include('Here is a list of users')
     end
   end
+
   describe 'GET /show' do
     before(:example) do
       get '/users/1'
