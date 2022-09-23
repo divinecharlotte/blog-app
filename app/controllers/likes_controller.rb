c8b9fa36fe2887499807e2c080df7d94c3f91d04
charlotte divine <divinemaina@gmail.com>
2022-09-22 21:52:41 -0700

    add create method in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,11 @@
 class LikesController < ApplicationController
-end
+    def create
+      @like = current_user.likes.new
+      @like.post_id = params[:post_id]
+      if @like.save
+        redirect_to user_post_likes_path(current_user, @like.post_id)
+      else
+        render :create
+      end
+    end
+  end
