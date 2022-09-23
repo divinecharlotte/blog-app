7999a5dfd31b4d22f22417501bfcefa40f55b409
charlotte divine <divinemaina@gmail.com>
2022-09-22 22:56:55 -0700

    add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,36 +7,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = Post.where(author_id: params[:user_id])
-    end
-    def show
-      @post = Post.find(params[:id])
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = current_user.posts.new(post_params)
-      respond_to do |format|
-        format.html do
-          if @post.save
-            redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
-          else
-            render :new
-          end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+        else
+          render :new
         end
       end
     end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :text, :likes_counter, :comments_counter)
-    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :likes_counter, :comments_counter)
   end
 end
