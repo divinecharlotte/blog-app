45904c9392435e0e90dbaf78696d6930049e2bbb
charlotte divine <divinemaina@gmail.com>
2022-09-27 00:10:49 -0700

    fix linters error

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Test Post show', type: :feature do
   before :each do
     @user = User.create(name: 'Lilly',
-                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.', post_counter: 0)
+                        photo: 'https://unsplash.com/photos/l-T-LpQnNRg', bio: 'Teacher from Poland.', post_counter: 0)
 
     @post = Post.create(author: @user, title: 'Post 1', text: 'This is my new post', comments_counter: 0,
                         likes_counter: 0)
--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.feature 'User Index Page', type: :feature do
   before :each do
     @user = User.create(name: 'Lilly',
-                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.', post_counter: 0)
+                        photo: 'https://unsplash.com/photos/l-T-LpQnNRg', bio: 'Teacher from Poland.', post_counter: 0)
 
     visit user_path(@user)
   end
